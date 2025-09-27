ce67b70fc9197065b26221cf4b6316c94cf033bb
Theodore Wolff <work@theo.gg>
2025-09-27 13:43:28 +0300

    removed gulp concat usage and functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 const gulp = require('gulp');
-const fs = require('fs');
-const path = require('path');
 const plumber = require('gulp-plumber');
 const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const gulpIf = require('gulp-if');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
-const concat = require('gulp-concat');
 const { createGulpEsbuild } = require('gulp-esbuild');
 const esbuild = createGulpEsbuild({ incremental: false });
 
@@ -35,33 +32,6 @@ function stylesMain(){
     .pipe(gulp.dest(paths.outCss));
 }
 
-function stylesSections(){
-  return gulp.src(['scss/sections/**/*.scss'], { allowEmpty: true })
-    .pipe(plumber())
-    .pipe(gulpIf(!isProd, sourcemaps.init()))
-    .pipe(sass.sync({ outputStyle: 'expanded' }))
-    .pipe(postcss([ require('autoprefixer')(), ...(isProd ? [require('cssnano')()] : []) ]))
-    .pipe(concat('sections.css'))
-    .pipe(rename({ basename: 'sections', suffix: '.min' }))
-    .pipe(gulpIf(!isProd, sourcemaps.write('.')))
-    .pipe(gulp.dest(paths.outCss));
-}
-
-function stylesPages(){
-  return gulp.src(['scss/pages/*.scss'], { allowEmpty: true })
-    .pipe(plumber())
-    .pipe(gulpIf(!isProd, sourcemaps.init()))
-    .pipe(sass.sync({ outputStyle: 'expanded' }))
-    .pipe(postcss([ require('autoprefixer')(), ...(isProd ? [require('cssnano')()] : []) ]))
-    .pipe(rename(function (p) {
-      p.dirname = 'pages';
-      p.basename = p.basename + '.min';
-      p.extname = '.css';
-    }))
-    .pipe(gulpIf(!isProd, sourcemaps.write('.')))
-    .pipe(gulp.dest(paths.outCss));
-}
-
 function scripts(){
   return gulp.src(['src/js/*.js'], { allowEmpty: true })
     .pipe(plumber())
@@ -69,20 +39,13 @@ function scripts(){
                     outdir: paths.outJs, entryNames:'[name]', legalComments:'none', minify:isProd }));
 }
 
-function watchAll(){
-  gulp.watch('scss/main.scss', stylesMain);
-  gulp.watch('scss/sections/**/*.scss', stylesSections);
-  gulp.watch('scss/pages/*.scss', stylesPages);
+function watchAll(){ 
+  gulp.watch(paths.scss, stylesMain);
   gulp.watch(paths.js, scripts);
 }
 
-const dev = gulp.series(clean, gulp.parallel(stylesMain, stylesSections, stylesPages, scripts));
-const build = gulp.series(() => { process.env.NODE_ENV='production'; return Promise.resolve(); },
-  clean, gulp.parallel(stylesMain, stylesSections, stylesPages, scripts));
+const dev = gulp.series(clean, gulp.parallel(stylesMain, scripts));
+const build = gulp.series(() => { process.env.NODE_ENV='production'; return Promise.resolve(); }, clean, gulp.parallel(stylesMain, scripts));
 
-exports.clean = clean;
-exports.styles = gulp.parallel(stylesMain, stylesSections, stylesPages);
-exports.scripts = scripts;
-exports.dev = dev;
-exports.build = build;
-exports.watch = gulp.series(dev, watchAll);
+exports.clean = clean; exports.styles = stylesMain; exports.scripts = scripts;
+exports.dev = dev; exports.build = build; exports.watch = gulp.series(dev, watchAll);
