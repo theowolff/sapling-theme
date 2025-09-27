0b43b61bb9237360d067db0e3b88c13316682b11
Theodore Wolff <work@theo.gg>
2025-09-27 19:06:49 +0300

    minor comment syntax change

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
     // Constant for assets
     if(! defined('TWWP_DIST')) {
-        define('TWWP_DIST', get_stylesheet_directory_uri() . '/dist');
+        define('TWWP_DIST', get_template_directory_uri() . '/dist');
     }
 
-    // Wire extras - core files
+    // Wire extras: core files
     require_once __DIR__ . '/extras/helpers.php';
     require_once __DIR__ . '/extras/setup.php';
     require_once __DIR__ . '/extras/theme-functions.php';
